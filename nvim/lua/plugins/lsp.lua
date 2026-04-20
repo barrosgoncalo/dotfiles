a999d4aa5a6a235168b79c08d9a310dea23d0dee
barrosgoncalo <barrosgoncalobarros@gmail.com>
2026-04-20 15:23:47 +0100

    fix: add jdtls config default values, prevent waning suppression

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,36 +1,52 @@
 return {
-  "neovim/nvim-lspconfig",
-  dependencies = {
-     {
-      "folke/lazydev.nvim",
-      ft = "lua",
-      opts = {
-        library = {
-          { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        {
+            "folke/lazydev.nvim",
+            ft = "lua",
+            opts = {
+                library = {
+                    { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                },
+            },
         },
-      },
     },
-  },
-  config = function()
-    vim.lsp.config("lua_ls", {
-      settings = {
-        Lua = {},
-      },
-    })
-    vim.lsp.config("bufls", {})
+    config = function()
+        -- configs
+        vim.lsp.config("lua_ls", {
+            settings = {
+                Lua = {},
+            },
+        })
+        vim.lsp.config("jdtls", {
+            settings = {
+                java = {
+                    compiler = {
+                        problem = {
+                            forbiddenReference = "warning",
+                            missingSerialVersion = "warning",
+                            unusedLambdaParameter = "warning",
+                            uncheckedTypeOperation = "warning",
+                        }
+                    }
+                }
+            }
+        })
+        vim.lsp.config("bufls", {})
 
-    vim.lsp.enable('lua_ls')
-    vim.lsp.enable("jdtls")
-    vim.lsp.enable("bufls")
+        -- enables
+        vim.lsp.enable('lua_ls')
+        vim.lsp.enable("jdtls")
+        vim.lsp.enable("bufls")
 
-    vim.diagnostic.config({
-      virtual_text = true,
-      update_in_insert = true,
-      underline = true,
-    })
-    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = 'Code Actions' })
-    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { desc = 'LSP Rename' })
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = 'LSP Hover Documentation' })
-    vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show Line Diagnostics' })
-  end
+        vim.diagnostic.config({
+            virtual_text = true,
+            update_in_insert = true,
+            underline = true,
+        })
+        vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = 'Code Actions' })
+        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { desc = 'LSP Rename' })
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = 'LSP Hover Documentation' })
+        vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show Line Diagnostics' })
+    end
 }
