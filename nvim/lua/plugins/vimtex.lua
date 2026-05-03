4d368d5e572481916aaeebd58f628cf87fb0a7d4
barrosgoncalo <barrosgoncalobarros@gmail.com>
2026-05-03 23:23:08 +0100

    n

--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -1,26 +1,34 @@
 return {
-  -- ==============================================================================
--- (Note: '\' assumes your <localleader> is set to the backslash key)
--- ==============================================================================
+    -- ==============================================================================
+    -- (Note: '\' assumes your <localleader> is set to the backslash key)
+    -- ==============================================================================
 
-  {
-    "lervag/vimtex",
-    lazy = false,
-    -- tag = "v2.15", -- uncomment to pin to a specific release
-    init = function()
-      vim.g.vimtex_view_method = "skim"
-      -- manage build auxiliary folders
-      -- vim.g.vimtex_compiler_latexmk = {
-      --   aux_dir = "aux", -- create a directory called aux that will contain all the auxiliary files
-      -- }
-    end
-  }
--- ==============================================================================
--- VimTeX Keybindings Quick Reference
--- ==============================================================================
--- \ll (Start/Stop Compilation)
--- \lv (View PDF)
--- \le (Errors)
--- \lc (Clean)
--- \li (Info)
+    {
+        "lervag/vimtex",
+        lazy = false,
+        -- tag = "v2.15", -- uncomment to pin to a specific release
+        init = function()
+            vim.g.vimtex_view_method = "skim"
+            -- manage build auxiliary folders
+            vim.g.vimtex_compiler_latexmk = {
+                options = {
+                    '-verbose',
+                    '-file-line-error',
+                    '-synctex=1',
+                    '-interaction=nonstopmode',
+                    '-shell-escape',
+                },
+                executable = 'latexmk',
+                hooks = {},
+            }
+        end
+    }
+    -- ==============================================================================
+    -- VimTeX Keybindings Quick Reference
+    -- ==============================================================================
+    -- \ll (Start/Stop Compilation)
+    -- \lv (View PDF)
+    -- \le (Errors)
+    -- \lc (Clean)
+    -- \li (Info)
 }
