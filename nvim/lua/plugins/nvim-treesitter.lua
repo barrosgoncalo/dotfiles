5f96ba8dbd8664dfac7e49eee6224f420e878dc0
barrosgoncalo <barrosgoncalobarros@gmail.com>
2026-09-21 18:32:00 +0100

    fix(treesitter): restore xml indentation broken by treesitter highlighting
    XmlIndentGet() relies on synID() for context detection, which stops
    working once vim.treesitter.start() takes over highlighting. Forcing
    syntax=xml keeps the legacy syntax engine active for indent purposes
    without affecting treesitter highlighting, mirroring the existing
    python workaround.

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,73 +1,80 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  branch = "main",
-  lazy = false,
-  build = ":TSUpdate",
-  config = function()
-    local install_dir = vim.fn.stdpath("data") .. "/site"
-    vim.opt.runtimepath:prepend(install_dir)
+    "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    lazy = false,
+    build = ":TSUpdate",
+    config = function()
+        local install_dir = vim.fn.stdpath("data") .. "/site"
+        vim.opt.runtimepath:prepend(install_dir)
 
-    vim.filetype.add({
-      extension = {
-        js = "javascriptreact",
-        tsx = "typescriptreact",
-      },
-    })
+        vim.filetype.add({
+            extension = {
+                js = "javascriptreact",
+                tsx = "typescriptreact",
+            },
+        })
 
-    require("nvim-treesitter").setup({
-      install_dir = install_dir,
-    })
+        require("nvim-treesitter").setup({
+            install_dir = install_dir,
+        })
 
-    require("nvim-treesitter").install({
-      "c",
-      "java",
-      "python",
-      "javascript",
-      "typescript",
-      "tsx",
-      "lua",
-      "vim",
-      "vimdoc",
-      "query",
-      "latex",
-    })
+        require("nvim-treesitter").install({
+            "c",
+            "java",
+            "python",
+            "javascript",
+            "typescript",
+            "tsx",
+            "lua",
+            "vim",
+            "vimdoc",
+            "query",
+            "latex",
+            "xml",
+        })
 
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = {
-        "c",
-        "java",
-        "python",
-        "javascript",
-        "javascriptreact",
-        "typescript",
-        "typescriptreact",
-        "lua",
-        "vim",
-        "vimdoc",
-        "query",
-      },
-      callback = function(args)
-        local lang_map = {
-          javascriptreact = "javascript",
-          typescriptreact = "tsx",
-        }
-        local lang = lang_map[args.match] or args.match
-        pcall(vim.treesitter.start, args.buf, lang)
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = {
+                "c",
+                "java",
+                "python",
+                "javascript",
+                "javascriptreact",
+                "typescript",
+                "typescriptreact",
+                "lua",
+                "vim",
+                "vimdoc",
+                "query",
+                "xml",
+            },
+            callback = function(args)
+                local lang_map = {
+                    javascriptreact = "javascript",
+                    typescriptreact = "tsx",
+                    xacml = "xml",
+                }
+                local lang = lang_map[args.match] or args.match
+                pcall(vim.treesitter.start, args.buf, lang)
 
-        local js_types = {
-          javascript = true, javascriptreact = true,
-          typescript = true, typescriptreact = true,
-        }
-        if js_types[args.match] then
-          vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-          vim.bo[args.buf].smartindent = false
-          vim.bo[args.buf].cindent = false
-        end
+                local js_types = {
+                    javascript = true, javascriptreact = true,
+                    typescript = true, typescriptreact = true,
+                }
+                if js_types[args.match] then
+                    vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                    vim.bo[args.buf].smartindent = false
+                    vim.bo[args.buf].cindent = false
+                end
 
-        if args.match == "python" then
-          vim.bo[args.buf].syntax = "python"
-        end
-      end,
-    })
-  end,
+                if args.match == "python" then
+                    vim.bo[args.buf].syntax = "python"
+                end
+
+                if args.match == "xml" then
+                    vim.bo[args.buf].syntax = "xml"
+                end
+            end,
+        })
+    end,
 }
